9f88de4fe254aad50872ce42fef8e493bc5b1f23
applemuffinhead <hkv5@hi.is>
2022-03-21 10:41:05 +0000

    Bjó til DayTrips class
    Gerði DayTrips class með constructor og getters.

---
+++ src/hi/verkefni/vidmot/DayTrips.java
@@ -0,0 +1,60 @@
+package hi.verkefni.vidmot;
+
+import java.util.Date;
+
+public class DayTrips {
+    //variables
+    private String bookDayTrip;
+    private String filterDayTrips;
+    private String chooseDayTrips;
+    private Integer dayTripsPrice;
+    private String changeDayTrip;
+    private Date dayTripDeparture;
+    private Date dayTripArrival;
+    private Integer dayTripDuration;
+
+    //constructor
+    public DayTrips(String bookDayTrip, String filterDayTrips, String chooseDayTrips, Integer dayTripsPrice, String changeDayTrip, Date dayTripDeparture, Date dayTripArrival, Integer dayTripDuration) {
+        this.bookDayTrip = bookDayTrip;
+        this.filterDayTrips = filterDayTrips;
+        this.chooseDayTrips = chooseDayTrips;
+        this.dayTripsPrice = dayTripsPrice;
+        this.changeDayTrip = changeDayTrip;
+        this.dayTripDeparture = dayTripDeparture;
+        this.dayTripArrival = dayTripArrival;
+        this.dayTripDuration = dayTripDuration;
+    }
+
+    //getters
+    public String getBookDayTrip() {
+        return bookDayTrip;
+    }
+
+    public String getFilterDayTrips() {
+        return filterDayTrips;
+    }
+
+    public String getChooseDayTrips() {
+        return chooseDayTrips;
+    }
+
+    public Integer getDayTripsPrice() {
+        return dayTripsPrice;
+    }
+
+    public String getChangeDayTrip() {
+        return changeDayTrip;
+    }
+
+    public Date getDayTripDeparture() {
+        return dayTripDeparture;
+    }
+
+    public Date getDayTripArrival() {
+        return dayTripArrival;
+    }
+
+    public Integer getDayTripDuration() {
+        return dayTripDuration;
+    }
+}
