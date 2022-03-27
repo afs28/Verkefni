fd60daa4fc9f42fd3e553ea18b3cd897ddac4c0f
applemuffinhead <hkv5@hi.is>
2022-03-27 19:35:16 +0000

    Tók út ónotað import
    Tók út ónotað import

--- src/main/java/hi/verkefni/vidmot/PaymentTest.java
+++ src/main/java/hi/verkefni/vidmot/PaymentTest.java
@@ -3,7 +3,6 @@ package hi.verkefni.vidmot;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PaymentTest {
