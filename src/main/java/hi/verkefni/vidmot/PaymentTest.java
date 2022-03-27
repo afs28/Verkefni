91bbd31d1f5147e0ebbd3cd4a0830bb3b9e86fb4
afs28 <afs28@hi.is>
2022-03-27 18:04:16 +0000

    test

--- src/main/java/hi/verkefni/vidmot/PaymentTest.java
+++ src/main/java/hi/verkefni/vidmot/PaymentTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PaymentTest {
     private Payment price1;
