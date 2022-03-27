a8eaac7cce787db2fa385ffb49172a1f6e68afd4
applemuffinhead <hkv5@hi.is>
2022-03-27 16:55:31 +0000

    Gerði Payment class og PaymentTest class
    Bætti við Payment class og PaymentTest class til að testa með JUnit

---
+++ src/hi/verkefni/vidmot/Payment.java
@@ -0,0 +1,16 @@
+package hi.verkefni.vidmot;
+
+public class Payment {
+    //variables
+    private Integer price;
+
+    //constructor
+    public Payment(Integer price) {
+        this.price = price;
+    }
+
+    //getters
+    public Integer getPrice() {
+        return price;
+    }
+}
---
+++ src/hi/verkefni/vidmot/PaymentTest.java
@@ -0,0 +1,31 @@
+package hi.verkefni.vidmot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PaymentTest {
+    private Payment price1;
+    private Payment price2;
+
+    @Before
+    public void setUp() {
+        price1 = new Payment(2000);
+        price2 = new Payment(40000);
+    }
+
+    @After
+    public void tearDown() {
+        price1 = null;
+        price2 = null;
+    }
+
+    @Test
+    public void testPrice() {
+        //fékk villu þegar ég sleppti Integer.valueOf(), lagaðist þegar ég bætti því við.
+        assertEquals(Integer.valueOf(2000), price1.getPrice());
+        assertEquals(Integer.valueOf(40000), price2.getPrice());
+    }
+
+}
