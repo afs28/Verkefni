518dd7e3d3945aa7cbe619dfb439ced2477de9c2
afs28 <afs28@hi.is>
2022-04-13 15:59:04 +0000

    Set up a minor interface for my account

--- src/main/java/hi/verkefni/vidmot/Main.java
+++ src/main/java/hi/verkefni/vidmot/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/MainMenu.fxml"));
         primaryStage.setTitle("Best META Search Engine");
-        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
