bbc74bc9381ce4712187def8a6d5a569c2882a03
applemuffinhead <hkv5@hi.is>
2022-04-15 15:56:30 +0000

    Setti scroll pane utan um allt í viðmótinu. Lét controller.java implements Initializable og í SceneBuilder þá gerði ég smá viðmót fyrir Flights

--- src/main/java/hi/verkefni/vidmot/Controller.java
+++ src/main/java/hi/verkefni/vidmot/Controller.java
@@ -1,4 +1,18 @@
 package hi.verkefni.vidmot;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/hi/verkefni/vidmot/Main.java
+++ src/main/java/hi/verkefni/vidmot/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/MainMenu.fxml"));
         primaryStage.setTitle("Best META Search Engine");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root, 1000, 500));
         primaryStage.show();
     }
 
