8e187b519f53538dea4df75b204790b3852593bc
applemuffinhead <hkv5@hi.is>
2022-04-19 19:19:40 +0000

    Er ennþá að vinna í útlitinu.

--- src/main/java/hi/verkefni/vidmot/Controller.java
+++ src/main/java/hi/verkefni/vidmot/Controller.java
@@ -1,14 +1,11 @@
 package hi.verkefni.vidmot;
 
-import hi.verkefni.vidmot.DayTrip.DayTrip;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
